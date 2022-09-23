e608bb4e6584a6837e611cabbbba5dd9f745c32d
Makario1337 <74052988+Makario1337@users.noreply.github.com>
2022-09-23 13:56:06 +0200

    Fixed Curl and Perl syntax problems

--- ERA.sh
+++ ERA.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 scriptVersion="1.0.0"
 ArtistsToWatch=$(jq '.artists[]' /config/artists.json)
+header='--header=User-Agent: Mozilla/5.0 (Windows NT 6.0) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.97 Safari/537.11'
+# Make sure Audiobooks path is mounted in container under /audiobooks!
 
 if [ -z "$lidarrUrl" ] || [ -z "$lidarrApiKey" ]; then
 	lidarrUrlBase="$(cat /config/config.xml | xq | jq -r .Config.UrlBase)"
@@ -30,6 +32,8 @@ log " Docker Version: $dockerVersion"
 log "-----------------------------------------------------------------------------"
 log " Donate to the original creator: https://github.com/sponsors/RandomNinjaAtk"
 log " Original Project: https://github.com/RandomNinjaAtk/docker-lidarr-extended"
+log " Extended Artist adder can be found under: "
+log " https://github.com/Makario1337/ExtendedReleaseAdder"
 log "-----------------------------------------------------------------------------"
 sleep 5
 log ""
@@ -53,37 +57,43 @@ jq '.' |
 cut -c 2- |
 head -c -2) 
 curl -X POST "$lidarrUrl/api/v1/album?apikey="$lidarrApiKey"" -H  "accept: text/plain" -H  "Content-Type: application/json" -d "$lidarrAlbumSearch" 
-} 
-
+sleep 0.5
+}
 SearchRelease(){
-    ReleaseName=$(wget --timeout=0 -q -O - "https://musicbrainz.org/ws/2/release-group/$1" | grep -oP "<title>.*</title>" | sed 's/<title>//g' | head -c -9 | sed 's/\&amp;/\&/g')
+    ReleaseName=$(wget "$header" --timeout=0 -q -O - "https://musicbrainz.org/ws/2/release-group/$1" | grep -o "<title>.*</title>" | sed 's/<title>//g' | head -c -9 | sed 's/\&amp;/\&/g')
     log "Adding :: $artist :: $ReleaseName"
     AddReleaseToLidarr $1 &> /dev/null
     sleep 0.5
 }
-
 SearchAllReleasesForArtist() {
 offset=0
 while [ $offset -le 500 ]
 do
     offset=$(( $offset + 100 ))
-    SearchAllReleasesForArtist=$(wget --timeout=0 -q -O - "https://musicbrainz.org/ws/2/release-group/?artist="$1"&limit=100&offset=$offset&fmt=json&type=other&secondary_type="audio%20drama"")
+    sleep 3
+    SearchAllReleasesForArtist=$(wget "$header" --timeout=0 -q -O - "https://musicbrainz.org/ws/2/release-group/?artist="$1"&limit=100&offset=$offset&fmt=json&type=other&secondary_type="audio%20drama"")
     lines=$(echo $SearchAllReleasesForArtist | jq '."release-groups"[]."id"')
-    for line in $lines
-    do
-        trim=$(echo $line | cut -c 2- | head -c -2)
-        SearchRelease $trim 
-    done
+    if [ -z "$lines" ]
+    then
+        log "ERROR :: Did not find matching release , skipping... "
+        log "ERROR :: Make sure the wanted items are listed under Other + Audio Drama on Musicbrainz"
+        offset=$(( $offset + 1337 ))
+    else
+        for line in $lines
+        do
+            trim=$(echo $line | cut -c 2- | head -c -2)
+            SearchRelease $trim 
+        done
+    fi
 done
 
 }
-
 ArtistLookup() {
 search=$(echo $1 | sed 's/"//g')
-artist=$(wget --timeout=0 -q -O - "https://musicbrainz.org/artist/$search" |perl -l -0777 -ne 'print $1 if /<title.*?>\s*(.*?)(?: - MusicBrainz)?\s*<\/title/si'|sed 's/\&amp;/\&/g')
-sleep 2
+artist=$(wget "$header" --timeout=0 -q -O - "https://musicbrainz.org/ws/2/artist/$search" | grep -o "<name>.*</name>" | sed 's/<name>//' | sed 's/<\/name>.*/‎/')
+sleep 5
 log "Adding :: $artist"
-SearchAllReleasesForArtist $1 $artist
+SearchAllReleasesForArtist $search
 }
 
 if [ -z "$ArtistsToWatch" ]
@@ -93,7 +103,6 @@ then
 else
     for str in ${ArtistsToWatch[@]}; do
       ArtistLookup $str 
-      sleep 5
     done
 fi
 
